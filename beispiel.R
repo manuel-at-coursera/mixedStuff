b4cfd5db1cd6afb17b6e59e5a40a17e3280dec09
manuel.at.coursera <manuel.at.coursera@gmail.com>
2014-12-01 16:25:51 +0100

    beispiel.R
    Beispiel Datei für Diana

---
+++ beispiel.R
@@ -0,0 +1,45 @@
+data <- read.csv("activity.csv")
+
+head(data)
+
+data
+
+# Loading and preprocessing the data
+setwd("foo/bar")
+data <- read.csv("activity.csv")
+
+# What is mean total number of steps taken per day?
+library(ggplot2)
+with(data, hist(steps, breaks="date"))
+
+
+# What is the average daily activity pattern?
+
+# Imputing missing values
+
+# Are there differences in activity patterns between weekdays and weekends?
+
+# 
+
+
+task1 <- aggregate(data$steps ~ data$date, data=data, FUN=sum)
+stepsPerDay <- aggregate(data$steps ~ data$date, data=data, FUN=sum)
+qplot(task1[,2])
+
+binwidth = 10
+
+hist(task1[,2], 
+     breaks=seq(min(task1[,2]),max(task1[,2]),l=length(task1[,2])+1))
+?hist
+
+hist(aggregate(data$steps ~ data$date, data=data, FUN=sum)[,2], breaks="FD")
+
+length(task1[,2])
+
+hist(task1[,2])
+
+x <- rnorm(100)
+mx <- mean(x)
+hist(x)
+abline(v = mx, col = "blue", lwd = 2)
+
